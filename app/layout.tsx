d7a0a36114ef067243d1a67fa768f87b71f7966d
roshanbvadassery <roshanbvadassry@gmail.com>
2025-08-05 04:48:04 +0530

    social sharing image

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,20 @@ export const metadata: Metadata = {
     description: 'Join the nationwide speed coding competition. We tour every city to find the fastest, most skilled programmers.',
     type: 'website',
     url: 'https://hyperthon.org',
+    images: [
+      {
+        url: '/og-image.svg',
+        width: 1200,
+        height: 630,
+        alt: 'Hyperthon - The Ultimate Speed Coding Competition - India Tour 2025',
+      }
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Hyperthon.org - The Ultimate Coding Competition Tour',
     description: 'Join the nationwide speed coding competition. We tour every city to find the fastest, most skilled programmers.',
+    images: ['/og-image.svg'],
   }
 };
 
